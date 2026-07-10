8a5e4ced3c4cb855548335367ce036327552c558
robsoncolonia846 <robsoncolonia846@gmail.com>
2026-07-10 15:21:28 -0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-﻿const CACHE_NAME = "dieta-controlada-pwa-v20";
+﻿const CACHE_NAME = "dieta-controlada-pwa-v22";
 
 const APP_SHELL = [
   "./",
@@ -56,5 +56,7 @@ self.addEventListener("fetch", (event) => {
     })
   );
 });
+
+
 
 
