84e0508f1759687425aa13a603c96d3ad0dea48a
robsoncolonia846 <robsoncolonia846@gmail.com>
2026-08-11 07:05:26 -0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "dieta-controlada-pwa-10.08.2026_Rev.40";
+const CACHE_NAME = "dieta-controlada-pwa-11.08.2026_Rev.43";
 
 importScripts("./firebase-config.js");
 importScripts("https://www.gstatic.com/firebasejs/10.13.2/firebase-app-compat.js");
