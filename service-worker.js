048a1eed7cdedec6b0e66c390487f92fdc7aa78f
robsoncolonia846 <robsoncolonia846@users.noreply.github.com>
2026-08-23 10:41:16 -0300

    Adiciona variacao diaria de peso

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "dieta-controlada-pwa-23.08.2026_Rev.10";
+const CACHE_NAME = "dieta-controlada-pwa-23.08.2026_Rev.11";
 
 importScripts("./firebase-config.js");
 importScripts("https://www.gstatic.com/firebasejs/10.13.2/firebase-app-compat.js");
